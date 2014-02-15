db204a36605bd6e0d488c3b5e21a4a245149895e
Felix246 <felix.rindt@gmail.com>
2014-02-15 20:53:20 +0100

    toll

--- zweidemo1/src/zweidemo1/Playground.java
+++ zweidemo1/src/zweidemo1/Playground.java
@@ -2,6 +2,7 @@ package zweidemo1;
 
 public class Playground {
 
-	boolean[] validField = new boolean[16*16];
+	final int height=16, width=16;
+	boolean[] validField = new boolean[height*width];
 	
 }
