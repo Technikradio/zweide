ab3d3596ffd45d2f7472d111d37cdbd2cf3c2b1f
Felix246 <felix.rindt@gmail.com>
2014-02-15 20:20:02 +0100

    lala

--- zweidemo1/src/zweidemo1/Player.java
+++ zweidemo1/src/zweidemo1/Player.java
@@ -1,5 +1,34 @@
 package zweidemo1;
 
 public class Player {
-	private String name="Player";
+	private String name = "Player";
+	private int posx = 0, posy = 0;
+
+	Player() {
+		initPlayer("", 0, 0);
+	}
+
+	Player(String name) {
+		initPlayer(name, 0, 0);
+	}
+
+	Player(int x, int y) {
+		initPlayer("", x, y);
+	}
+
+	Player(String name, int x, int y) {
+		initPlayer(name, x, y);
+	}
+
+	private void initPlayer(String name, int x, int y) {
+		this.posx = x;
+		this.posy = y;
+		this.name = name;
+	}
+	
+	void move(int x, int y) {
+		this.posx += 16*x;
+		this.posy += 16*y;
+	}
+	
 }
