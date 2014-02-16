fa14461adbe446c4c05ec80901aced84503be156
Felix246 <felix.rindt@gmail.com>
2014-02-16 12:33:20 +0100

    delete 1

--- zweidemo1/src/zweidemo1/Properties.java
+++
@@ -1,8 +0,0 @@
-package zweidemo1;
-
-public static class Properties{
-	public static final String name = "Zweidemo";
-	public static final int resolutionW = 1024;
-	public static final int resolutionH = 380;
-	public static final boolean resizable = false;
-}
