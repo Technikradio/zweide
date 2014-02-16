2a9580e623a17d3cd2cdbdd5e3bcd0197ba62159
Doralitze <leondietrich@leons-imac.speedport_w_921v_1_24_000>
2014-02-16 10:47:39 +0100

    Redone my plans

---
+++ zweidemo1/src/zweidemo1/Properties.java
@@ -0,0 +1,7 @@
+package zweidemo1;
+
+public static class Properties{
+	public static String name = "Zweidemo";
+	public static int resolutionW = 1024;
+	public static int resolutionH = 380;
+}
--- zweidemo1/src/zweidemo1/Rendering.java
+++ zweidemo1/src/zweidemo1/Rendering.java
@@ -1,6 +1,11 @@
 package zweidemo1;
-public static class Rendering{
-	public static ImageIcon render(){
-		
+import javax.swing.JFrame;
+
+public class Rendering extends JFrame{
+	public Rendering(){
+		add(new Board());
+		setTitle(Properties.name);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(Properties.resulotionW, Properties.resulotionH);
 	}
 }
