a3507d9ea68a7042c5c89ed37d879697d9f24512
Doralitze <technikradio@icloud.com>
2014-03-24 17:24:28 +0100

    Update Level.java
    Add some useful stuff

--- Tompong/src/zweide/tompong/Level.java
+++ Tompong/src/zweide/tompong/Level.java
@@ -1,3 +1,12 @@
+package zweide.tompong;
 public class Level{
-
+  public int minUpSpeedPlayer;
+  public int maxUpSpeedPlayer;
+  public int minUpSpeedBoss;
+  public int maxSpeedBoss;
+  public int minUpSpeedPlayer;
+  public int maxUpSpeedPlayer;
+  public int minUpSpeedBoss;
+  public int maxSpeedBoss;
+  public int ballSpeed;
 }
