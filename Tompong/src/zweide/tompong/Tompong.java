5c0de50ceb87b6b38fe2670c8d0e8b7ae86d902c
Doralitze <technikradio@icloud.com>
2014-03-26 08:17:30 +0100

    Implement mothods for the actionhandler
    Next: add the actionlistener itself

--- Tompong/src/zweide/tompong/Tompong.java
+++ Tompong/src/zweide/tompong/Tompong.java
@@ -3,9 +3,36 @@ package zweide.tompong;
 
 public class Tompong {
 
+	static GameFrame frame;
+	static Menu menu;
+
 	public static void main(String[] args) {
-		GameFrame frame = new GameFrame();
+		frame = new GameFrame();
+		menu = new Menu();
+		menu.setVisible(true);
+	}
+	
+	public static void menu_OnEasyClick(){
+		Resources.levelIndex = 0;
+		menu.setVisible(false);
+		frame.setVisible(true);
+	}
+	
+	public static void menu_OnMediumClick(){
+		Recources.levelIndex = 1;
+		menu.setVisible(false);
 		frame.setVisible(true);
 	}
 
+	public static void menu_OnHardClick(){
+		Recources.levelIndex = 2;
+		menu.setVisible(false);
+		frame.setVisible(true);
+	}
+	
+	public static void menu_OnCostumClick(){
+		//TODO implement costum menu
+		menu.setVisible(false);
+		frame.setVisible(true);
+	}
 }
