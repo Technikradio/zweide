a23578deace639051b3afce521266ff592fc70c7
Doralitze <technikradio@icloud.com>
2014-03-26 11:10:04 +0100

    Create Powerup.java

---
+++ Tompong/src/zweide/tompong/Powerup.java
@@ -0,0 +1,16 @@
+package zweide.tompong
+
+import java.awt.Graphics;
+
+public class Powerup{
+
+  public final byte type;
+
+  public Powerup(){
+  
+  }
+  
+  public void draw(Graphics g){
+    
+  }
+}
