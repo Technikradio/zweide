36191d5c3524150aa92f6955adb886d1638bddd4
Doralitze <leondietrich@leons-imac.speedport_w_921v_1_24_000>
2014-04-03 17:25:12 +0200

    Ok, it´s done
    Next: AI, Boss & Video

--- Tompong/src/zweide/tompong/PongPanel.java
+++ Tompong/src/zweide/tompong/PongPanel.java
@@ -12,207 +12,216 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class PongPanel extends JPanel implements Runnable {
 
-  private Player playerL, playerR;
-  private Ball mainBall;
-  private int frameWidth, frameHeight;
-  private Level currentLevel;
+	private Player playerL, playerR;
+	private Ball mainBall;
+	private int frameWidth, frameHeight;
+	private Level currentLevel;
+	private Powerup currentPowerup = PowerupRegister.getNext();
 
-  public PongPanel(int x, int y) {
+	public PongPanel(int x, int y) {
 
-    frameWidth = x;
-    frameHeight = y;
+		frameWidth = x;
+		frameHeight = y;
 
-    setBackground(Color.black);
+		setBackground(Color.black);
 
-    playerL = new Player();
-    playerR = new Player();
-    mainBall = new Ball(16, frameWidth / 2 - 8, frameHeight / 2 - 8);
+		playerL = new Player();
+		playerR = new Player();
+		mainBall = new Ball(16, frameWidth / 2 - 8, frameHeight / 2 - 8);
 
-    playerL.setPosBounds(0, frameHeight - 28);
-    playerR.setPosBounds(0, frameHeight - 28);
-    mainBall.setPosBounds(0, frameHeight - 28);
+		playerL.setPosBounds(0, frameHeight - 28);
+		playerR.setPosBounds(0, frameHeight - 28);
+		mainBall.setPosBounds(0, frameHeight - 28);
 
-    playerL.setPos((frameHeight - 28 - playerL.getBarHeight()) / 2);
-    playerR.setPos((frameHeight - 28 - playerR.getBarHeight()) / 2);
+		playerL.setPos((frameHeight - 28 - playerL.getBarHeight()) / 2);
+		playerR.setPos((frameHeight - 28 - playerR.getBarHeight()) / 2);
 
-    playerL.setKEYCODE_UP(38);
-    playerL.setKEYCODE_DOWN(40);
+		playerL.setKEYCODE_UP(38);
+		playerL.setKEYCODE_DOWN(40);
 
-    playerR.setKEYCODE_UP(107);
-    playerR.setKEYCODE_DOWN(10);
+		playerR.setKEYCODE_UP(107);
+		playerR.setKEYCODE_DOWN(10);
 
-    setCurrentLevel(Resources.getDefaultLevel());
+		setCurrentLevel(Resources.getDefaultLevel());
 
-    createThread();
-  }
-  
-  public void createThread(){
-	  Resources.gameThread = new Thread(this);
-	  Resources.gameThread.start();
-  }
+		createThread();
+	}
 
-  @Override
-  public void run() {
+	public void createThread() {
+		Resources.gameThread = new Thread(this);
+		Resources.gameThread.start();
+	}
 
-    // initialize
-    long startTime, elapsedTime;
-    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-    mainBall.setVerticalMotion((int) ((double) (-currentLevel.ballSpeed / 2) + (double) Math
-        .random() * (double) (currentLevel.ballSpeed)));
-    if (Math.random() >= 0.5)
-      mainBall.setHorizontalDirection(1);
-    else
-      mainBall.setHorizontalDirection(-1);
+	@Override
+	public void run() {
 
-    // wait
-    try {
-      Thread.sleep(100);
-    } catch (InterruptedException e1) {
-      e1.printStackTrace();
-    }
+		// initialize
+		long startTime, elapsedTime;
+		SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+		mainBall.setVerticalMotion((int) ((double) (-currentLevel.ballSpeed / 2) + (double) Math
+				.random() * (double) (currentLevel.ballSpeed)));
+		if (Math.random() >= 0.5)
+			mainBall.setHorizontalDirection(1);
+		else
+			mainBall.setHorizontalDirection(-1);
 
-    System.out.println("Entering Gameloop");
+		// wait
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 
-    while (GBAE.running == true) {
+		System.out.println("Entering Gameloop");
 
-      // Get Calculation start timing
-      startTime = System.currentTimeMillis();
+		while (GBAE.running == true) {
 
-      // Player
-      playerR.setPos(playerR.getPos() + currentLevel.barSpeed
-          * playerR.getMovement());
-      playerL.setPos(playerL.getPos() + currentLevel.barSpeed
-          * playerL.getMovement());
+			// Get Calculation start timing
+			startTime = System.currentTimeMillis();
 
-      // MainBall
-      mainBall.setHoriPos(mainBall.getHoriPos()
-          + mainBall.getHorizontalDirection()
-          * mainBall.getHorizontalDistance());
-      mainBall.setVertPos(mainBall.getVertPos()
-          + mainBall.getVerticalMotion());
+			// Player
+			playerR.setPos(playerR.getPos() + currentLevel.barSpeed
+					* playerR.getMovement());
+			playerL.setPos(playerL.getPos() + currentLevel.barSpeed
+					* playerL.getMovement());
 
-      // Hitbox (?? how was it called??)
-      if (new Rectangle(mainBall.getHoriPos(), mainBall.getVertPos(),
-          mainBall.getSize(), mainBall.getSize()).intersects(27,
-          playerL.getPos(), 10, playerL.getBarHeight())) {
-        // Ball is in left player
-        mainBall.setHorizontalDirection(1);
-        mainBall.setVerticalMotion((int) Math.round(mainBall
-            .getVerticalMotion()
-            - (double) ((playerL.getPos() + playerL.getBarHeight() / 2) - (mainBall
-                .getVertPos() + mainBall.getSize() / 2))
-            / playerL.getBarHeight() * currentLevel.ballSpeed));
-      }
-      if (new Rectangle(mainBall.getHoriPos(), mainBall.getVertPos(),
-          mainBall.getSize(), mainBall.getSize()).intersects(
-          frameWidth - 37, playerR.getPos(), 10,
-          playerR.getBarHeight())) {
-        // Ball is in right player
-        mainBall.setHorizontalDirection(-1);
-        mainBall.setVerticalMotion((int) Math.round(mainBall
-            .getVerticalMotion()
-            - (double) ((playerR.getPos() + playerR.getBarHeight() / 2) - (mainBall
-                .getVertPos() + mainBall.getSize() / 2))
-            / playerR.getBarHeight() * currentLevel.ballSpeed));
-      }
-      if (mainBall.getVertPos() + mainBall.getSize() > mainBall
-          .getUpperPosBounds()) {
-        // ball is at low border
-        mainBall.setVerticalMotion(-1
-            * Math.abs(mainBall.getVerticalMotion()));
-      }
-      if (mainBall.getVertPos() < mainBall.getLowerPosBounds()) {
-        // ball is at low border
-        mainBall.setVerticalMotion(Math.abs(mainBall
-            .getVerticalMotion()));
-      }
-      if (mainBall.getHoriPos() < 0) {
-        // Ball left goal
-        mainBall.setPos(frameWidth / 2 - 8, frameHeight / 2 - 8);
-        newBallParameter();
-        playerR.setScore(playerR.getScore() + 1);
-      }
-      if (mainBall.getHoriPos() + mainBall.getSize() > frameWidth) {
-        // Ball right goal
-        newBallParameter();
-        playerL.setScore(playerL.getScore() + 1);
-      }
+			// MainBall
+			mainBall.setHoriPos(mainBall.getHoriPos()
+					+ mainBall.getHorizontalDirection()
+					* mainBall.getHorizontalDistance());
+			mainBall.setVertPos(mainBall.getVertPos()
+					+ mainBall.getVerticalMotion());
 
-      // Repaint
-      repaint();
+			// Hitbox (?? how was it called??)
+			if (new Rectangle(mainBall.getHoriPos(), mainBall.getVertPos(),
+					mainBall.getSize(), mainBall.getSize()).intersects(27,
+					playerL.getPos(), 10, playerL.getBarHeight())) {
+				// Ball is in left player
+				mainBall.setHorizontalDirection(1);
+				mainBall.setVerticalMotion((int) Math.round(mainBall
+						.getVerticalMotion()
+						- (double) ((playerL.getPos() + playerL.getBarHeight() / 2) - (mainBall
+								.getVertPos() + mainBall.getSize() / 2))
+						/ playerL.getBarHeight() * currentLevel.ballSpeed));
+			}
+			if (new Rectangle(mainBall.getHoriPos(), mainBall.getVertPos(),
+					mainBall.getSize(), mainBall.getSize()).intersects(
+					frameWidth - 37, playerR.getPos(), 10,
+					playerR.getBarHeight())) {
+				// Ball is in right player
+				mainBall.setHorizontalDirection(-1);
+				mainBall.setVerticalMotion((int) Math.round(mainBall
+						.getVerticalMotion()
+						- (double) ((playerR.getPos() + playerR.getBarHeight() / 2) - (mainBall
+								.getVertPos() + mainBall.getSize() / 2))
+						/ playerR.getBarHeight() * currentLevel.ballSpeed));
+			}
+			if (new Rectangle(mainBall.getHoriPos(), mainBall.getVertPos(),
+					mainBall.getSize(), mainBall.getSize())
+					.intersects(currentPowerup)) {
+				currentPowerup.onEvent();
+				currentPowerup = null;
+				currentPowerup = PowerupRegister.getNext();
+			}
+			if (mainBall.getVertPos() + mainBall.getSize() > mainBall
+					.getUpperPosBounds()) {
+				// ball is at low border
+				mainBall.setVerticalMotion(-1
+						* Math.abs(mainBall.getVerticalMotion()));
+			}
+			if (mainBall.getVertPos() < mainBall.getLowerPosBounds()) {
+				// ball is at low border
+				mainBall.setVerticalMotion(Math.abs(mainBall
+						.getVerticalMotion()));
+			}
+			if (mainBall.getHoriPos() < 0) {
+				// Ball left goal
+				mainBall.setPos(frameWidth / 2 - 8, frameHeight / 2 - 8);
+				newBallParameter();
+				playerR.setScore(playerR.getScore() + 1);
+			}
+			if (mainBall.getHoriPos() + mainBall.getSize() > frameWidth) {
+				// Ball right goal
+				newBallParameter();
+				playerL.setScore(playerL.getScore() + 1);
+			}
 
-      // Calculate elapsed time
-      elapsedTime = System.currentTimeMillis() - startTime;
-      if (elapsedTime > 1) {
-        System.out.println("Frame Time high: " + elapsedTime
-            + " Time: " + timeFormat.format(new Date()));
-      }
+			// Repaint
+			repaint();
 
-      // Wait
-      try {
-        Thread.sleep((elapsedTime < 20) ? (20 - elapsedTime) : 1L);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }
-  }
+			// Calculate elapsed time
+			elapsedTime = System.currentTimeMillis() - startTime;
+			if (elapsedTime > 1) {
+				System.out.println("Frame Time high: " + elapsedTime
+						+ " Time: " + timeFormat.format(new Date()));
+			}
 
-  private void newBallParameter() {
-    mainBall.setVerticalMotion((int) ((double) (-currentLevel.ballSpeed / 2) + (double) Math
-        .random() * (double) (currentLevel.ballSpeed)));
-    mainBall.setPos(frameWidth / 2 - mainBall.getSize() / 2, frameHeight
-        / 2 - mainBall.getSize() / 2);
-    mainBall.setHorizontalDirection(-mainBall.getHorizontalDirection());
-  }
+			// Wait
+			try {
+				Thread.sleep((elapsedTime < 20) ? (20 - elapsedTime) : 1L);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-  @Override
-  protected void paintComponent(Graphics g) {
+	private void newBallParameter() {
+		mainBall.setVerticalMotion((int) ((double) (-currentLevel.ballSpeed / 2) + (double) Math
+				.random() * (double) (currentLevel.ballSpeed)));
+		mainBall.setPos(frameWidth / 2 - mainBall.getSize() / 2, frameHeight
+				/ 2 - mainBall.getSize() / 2);
+		mainBall.setHorizontalDirection(-mainBall.getHorizontalDirection());
+	}
 
-    // General
-    super.paintComponent(g);
-    g.setColor(Color.white);
+	@Override
+	protected void paintComponent(Graphics g) {
 
-    // Player
-    g.fillRect(27, playerL.getPos(), 10, playerL.getBarHeight());
-    g.fillRect(frameWidth - 37, playerR.getPos(), 10,
-        playerR.getBarHeight());
+		// General
+		super.paintComponent(g);
+		g.setColor(Color.white);
 
-    // Ball
-    g.fillRect(mainBall.getHoriPos(), mainBall.getVertPos(),
-        mainBall.getSize(), mainBall.getSize());
+		// Player
+		g.fillRect(27, playerL.getPos(), 10, playerL.getBarHeight());
+		g.fillRect(frameWidth - 37, playerR.getPos(), 10,
+				playerR.getBarHeight());
 
-    // dividing Line
-    for (int i = 0; i < frameHeight; i += 32) {
-      g.fillRect(getWidth() / 2 - 1, i, 2, 16);
-    }
+		// Ball
+		g.fillRect(mainBall.getHoriPos(), mainBall.getVertPos(),
+				mainBall.getSize(), mainBall.getSize());
 
-    // Score
-    g.drawString(Integer.toString(playerL.getScore()), 300, 100);
-    g.drawString(Integer.toString(playerR.getScore()), 500, 100);
+		// dividing Line
+		for (int i = 0; i < frameHeight; i += 32) {
+			g.fillRect(getWidth() / 2 - 1, i, 2, 16);
+		}
 
-  }
+		// Score
+		g.drawString(Integer.toString(playerL.getScore()), 300, 100);
+		g.drawString(Integer.toString(playerR.getScore()), 500, 100);
 
-  public Level getCurrentLevel() {
-    return currentLevel;
-  }
+		currentPowerup.Draw(g);
 
-  public void setCurrentLevel(Level newLevel) {
-    currentLevel = newLevel;
-    playerL.setBarHeight(currentLevel.barHeight);
-    playerR.setBarHeight(currentLevel.barHeight);
-    mainBall.setSpeed(currentLevel.ballSpeed);
-  }
+	}
 
-  public void keyInput(KeyEvent e, boolean active) {
-    if (e.getKeyCode() == playerR.getKEYCODE_UP())
-      playerR.setGoingUp(active);
-    else if (e.getKeyCode() == playerR.getKEYCODE_DOWN())
-      playerR.setGoingDown(active);
-    else if (e.getKeyCode() == playerL.getKEYCODE_UP())
-      playerL.setGoingUp(active);
-    else if (e.getKeyCode() == playerL.getKEYCODE_DOWN())
-      playerL.setGoingDown(active);
-  }
+	public Level getCurrentLevel() {
+		return currentLevel;
+	}
+
+	public void setCurrentLevel(Level newLevel) {
+		currentLevel = newLevel;
+		playerL.setBarHeight(currentLevel.barHeight);
+		playerR.setBarHeight(currentLevel.barHeight);
+		mainBall.setSpeed(currentLevel.ballSpeed);
+	}
+
+	public void keyInput(KeyEvent e, boolean active) {
+		if (e.getKeyCode() == playerR.getKEYCODE_UP())
+			playerR.setGoingUp(active);
+		else if (e.getKeyCode() == playerR.getKEYCODE_DOWN())
+			playerR.setGoingDown(active);
+		else if (e.getKeyCode() == playerL.getKEYCODE_UP())
+			playerL.setGoingUp(active);
+		else if (e.getKeyCode() == playerL.getKEYCODE_DOWN())
+			playerL.setGoingDown(active);
+	}
 
 }
-
--- Tompong/src/zweide/tompong/PowerupRegister.java
+++ Tompong/src/zweide/tompong/PowerupRegister.java
@@ -7,39 +7,47 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.ArrayList;
 
-@SuppressWarnings( "unused" )
+@SuppressWarnings("unused")
 public class PowerupRegister {
-	
+
 	static ArrayList<Powerup> powerups;
-	static int count = 0;
-	
-	static{
+	static int count = -1;
+
+	static {
 		powerups = new ArrayList<Powerup>();
-		java.lang.Class<java.lang.Object>[] classes = null/* = GetAllClasses() oder so?*/; //Get all classes of zweide.tompong
-		powerups.add(new zweide.tompong.powerups.LSD(Resources.sizeX / 2, Resources.sizeY / 2));
+		java.lang.Class<java.lang.Object>[] classes = null/*
+														 * = GetAllClasses()
+														 * oder so?
+														 */; // Get all classes
+																// of
+																// zweide.tompong
+		powerups.add(new zweide.tompong.powerups.LSD(Resources.sizeX / 2,
+				Resources.sizeY / 2));
+		count++;
 		/*
-		for(int i = 0; i <= classes.length; i++){
-			if(classes[i].getClass().isAnnotationPresent(RegisterPowerup.class)){
-				try {
-					powerups.add((Powerup) classes[i].newInstance());
-				} catch (InstantiationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-		*/
+		 * for(int i = 0; i <= classes.length; i++){
+		 * if(classes[i].getClass().isAnnotationPresent(RegisterPowerup.class)){
+		 * try { powerups.add((Powerup) classes[i].newInstance()); } catch
+		 * (InstantiationException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } catch (IllegalAccessException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } } }
+		 */
 	}
-	
-	public static Powerup getNext(){
-		if(count > 0) return (Powerup) powerups.toArray()[(int) Math.round(Math.random() * count - 1)]; else return null; // This should return an random item of powerups right?
+
+	public static Powerup getNext() {
+		if (count > -1)
+			return (Powerup) powerups.toArray()[(int) Math
+					.round((Math.random() * count))];
+		else
+			return new zweide.tompong.powerups.LSD(Resources.sizeX / 2,
+					Resources.sizeY / 2); // This should return an random item
+											// of powerups right?
 	}
-	
+
 	@Documented
 	@Target(ElementType.TYPE)
 	@Retention(RetentionPolicy.RUNTIME)
-	public static @interface RegisterPowerup{boolean notCommon() default false;}
+	public static @interface RegisterPowerup {
+		boolean notCommon() default false;
+	}
 }
